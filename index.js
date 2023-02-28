5d51c7c091fa30a360234e3972af26b328a6fb8c
Dean Marques <deanmarquesjr@gmail.com>
2023-02-27 23:50:31 -0800

    Third commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+const express = require('express');
+const PORT = 5555;
+const app = express();
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Pool Queue running on port: ${PORT}`);
+})
